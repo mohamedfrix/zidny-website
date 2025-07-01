2abac61a01dc88f39e147639613aee842a99a33a
mohamedfrix <mohamed.frihaoui@ensia.edu.dz>
2025-07-01 03:20:17 +0100

    refactor: add the feature section cards content

--- src/components/landing_page/FeaturesSection.tsx
+++ src/components/landing_page/FeaturesSection.tsx
@@ -3,6 +3,15 @@
 import {useLanguage} from "@/hooks/useLanguage";
 import {AspectRatio} from "@/components/ui/aspect-ratio";
 
+import Image from "next/image";
+
+import feature_1 from "@/assets/images/feature_1.png";
+import feature_2 from "@/assets/images/feature_2.png";
+import feature_3 from "@/assets/images/feature_3.png";
+import feature_4 from "@/assets/images/feature_4.png";
+import feature_5 from "@/assets/images/feature_5.png";
+import feature_6 from "@/assets/images/feature_6.png";
+
 
 function FeaturesSection () {
 
@@ -19,13 +28,54 @@ function FeaturesSection () {
 
                     <div className={`w-full max-w-[90%] md:max-w-[75%] lg:max-w-[60%] xl:max-w-[70%]`}>
                         <AspectRatio ratio={0.9} className={`w-full`}>
-                            <div className={`w-full h-full features-grid-template gap-x-2 gap-y-2 grid`}>
-                                <div className={`w-full h-full row-start-1 row-span-2 col-start-1 col-span-2 bg-card-1 rounded-lg`}></div>
-                                <div className={`w-full h-full row-start-1 row-span-1 col-start-3 col-span-4 bg-card-4 rounded-lg`}></div>
-                                <div className={`w-full h-full row-start-2 row-span-1 col-start-3 col-span-2 bg-card-4 rounded-lg`}></div>
-                                <div className={`w-full h-full row-start-2 row-span-1 col-start-5 col-span-2 bg-card-2 rounded-lg`}></div>
-                                <div className={`w-full h-full row-start-3 row-span-1 col-start-1 col-span-3 bg-card-4 rounded-lg`}></div>
-                                <div className={`w-full h-full row-start-3 row-span-1 col-start-4 col-span-3 bg-card-3 rounded-lg`}></div>
+                            <div className={`w-full h-full features-grid-template gap-x-1 gap-y-1 md:gap-x-2 md:gap-y-2 grid`}>
+                                <div className={`w-full h-full row-start-1 row-span-2 col-start-1 col-span-2 bg-card-1 rounded-[16px] md:rounded-[24px] overflow-hidden grid grid-rows-1 grid-cols-1`}>
+                                    <Image src={feature_1} alt="Feature Image" className={`w-full h-full object-cover row-start-1 col-start-1 z-0`} />
+                                    <div className={`md:hidden w-full h-full bg-white/10 backdrop-blur-lg z-10 row-start-1 col-start-1`}></div>
+                                    <div className={`row-start-1 col-start-1 z-20 w-full flex flex-col items-center px-2 py-5 sm:px-4 sm:py-8 lg:px-10 lg:py-16 gap-y-2 lg:gap-y-4`}>
+                                        <p className={`text-white font-bold text-[18px] sm:text-[32px] lg:text-[42px] font-outfit text-center`}>{t('features.featuresList.0.title')}</p>
+                                        <p className={`text-white sm:font-medium text-[10px] sm:text-[12px] lg:text-[16px] font-outfit text-center`}>{t('features.featuresList.0.description')}</p>
+                                    </div>
+                                </div>
+                                <div className={`w-full h-full row-start-1 row-span-1 col-start-3 col-span-4 bg-card-4 rounded-[16px] md:rounded-[24px] overflow-hidden grid grid-rows-1 grid-cols-1`}>
+                                    <Image src={feature_2} alt="Feature Image" className={`w-full h-full object-cover row-start-1 col-start-1 z-0`} />
+                                    <div className={`md:hidden w-full h-full bg-white/10 backdrop-blur-sm z-10 row-start-1 col-start-1`}></div>
+                                    <div className={`w-full row-start-1 col-start-1 z-20 self-end flex flex-col px-3 py-4 sm:px-6 sm:py-8 lg:px-8 lg:py-12 gap-y-1`}>
+                                        <p className={`text-[#0C224B] text-[16px] sm:text-[28px] lg:text-[42px] font-bold font-outfit`}>{t('features.featuresList.1.title')}</p>
+                                        <p className={`text-neutral-gray-2 text-[10px] sm:text-[14px] lg:text-[16px] sm:font-medium font-outfit`}>{t('features.featuresList.1.description')}</p>
+                                    </div>
+                                </div>
+                                <div className={`w-full h-full row-start-2 row-span-1 col-start-3 col-span-2 bg-card-4 rounded-[16px] md:rounded-[24px] overflow-hidden grid grid-rows-1 grid-cols-1`}>
+                                    <Image src={feature_3} alt="Feature Image" className={`w-full h-full object-cover row-start-1 col-start-1 z-0`} />
+                                    <div className={`md:hidden w-full h-full bg-white/10 backdrop-blur-sm z-10 row-start-1 col-start-1`}></div>
+                                    <div className={`w-full row-start-1 col-start-1 z-20 self-end flex flex-col px-3 py-4 sm:px-6 sm:py-6 lg:px-8 lg:py-10 gap-y-1`}>
+                                        <p className={`text-[#0C224B] text-[12px] sm:text-[18px] lg:text-[22px] xl:text-[32px] font-bold font-outfit`}>{t('features.featuresList.2.title')}</p>
+                                    </div>
+                                </div>
+                                <div className={`w-full h-full row-start-2 row-span-1 col-start-5 col-span-2 bg-card-2 rounded-[16px] md:rounded-[24px] overflow-hidden grid grid-rows-1 grid-cols-1`}>
+                                    <Image src={feature_4} alt="Feature Image" className={`w-full h-full object-cover row-start-1 col-start-1 z-0`} />
+                                    <div className={`md:hidden w-full h-full bg-white/10 backdrop-blur-sm z-10 row-start-1 col-start-1`}></div>
+                                    <div className={`row-start-1 col-start-1 z-20 w-full flex flex-col px-2 py-3 sm:px-6 sm:py-10 lg:px-4 lg:py-6 gap-y-1`}>
+                                        <p className={`text-white font-bold text-[16px] sm:text-[26px] lg:text-[36px] font-outfit`}>{t('features.featuresList.3.title')}</p>
+                                        <p className={`text-white sm:font-medium text-[8px] sm:text-[12px] lg:text-[14px] xl:text-[16px] font-outfit `}>{t('features.featuresList.3.description')}</p>
+                                    </div>
+                                </div>
+                                <div className={`w-full h-full row-start-3 row-span-1 col-start-1 col-span-3 bg-card-4 rounded-[16px] md:rounded-[24px] overflow-hidden grid grid-rows-1 grid-cols-1`}>
+                                    <Image src={feature_5} alt="Feature Image" className={`w-full h-full object-cover row-start-1 col-start-1 z-0`} />
+                                    <div className={`md:hidden w-full h-full bg-white/10 backdrop-blur-sm z-10 row-start-1 col-start-1`}></div>
+                                    <div className={`w-full row-start-1 col-start-1 z-20 self-start flex flex-col px-3 py-6 sm:px-6 sm:py-10 lg:px-8 lg:py-14 gap-y-1`}>
+                                        <p className={`text-[#0C224B] text-[16px] sm:text-[28px] lg:text-[42px] font-bold font-outfit`}>{t('features.featuresList.4.title')}</p>
+                                        <p className={`text-gray-900 md:text-neutral-gray-2 text-[10px] sm:text-[14px] lg:text-[16px] sm:font-medium font-outfit`}>{t('features.featuresList.4.description')}</p>
+                                    </div>
+                                </div>
+                                <div className={`w-full h-full row-start-3 row-span-1 col-start-4 col-span-3 bg-card-3 rounded-[16px] md:rounded-[24px] overflow-hidden grid grid-rows-1 grid-cols-1`}>
+                                    <Image src={feature_6} alt="Feature Image" className={`w-full h-full object-cover row-start-1 col-start-1 z-0`} />
+                                    <div className={`md:hidden w-full h-full bg-white/10 backdrop-blur-sm z-10 row-start-1 col-start-1`}></div>
+                                    <div className={`w-full row-start-1 col-start-1 z-20 self-end flex flex-col px-2 py-2 sm:px-6 sm:py-8 lg:px-8 lg:py-12 gap-y-1`}>
+                                        <p className={`text-[#0C224B] text-[16px] sm:text-[28px] lg:text-[42px] font-bold font-outfit`}>{t('features.featuresList.5.title')}</p>
+                                        <p className={`text-gray-900 xl:text-neutral-gray-2 text-[9px] sm:text-[14px] lg:text-[16px] sm:font-medium font-outfit`}>{t('features.featuresList.5.description')}</p>
+                                    </div>
+                                </div>
                             </div>
                         </AspectRatio>
                     </div>
