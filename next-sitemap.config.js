9e28194b5579717e3431d126ea4e60daf4e5f75a
Tabti Ali <ma_tabti@esi.dz>
2025-07-18 19:40:45 +0100

    sitemap and robots files fixed

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: "http://localhost:3000",
+  generateRobotsTxt: true, // Génère aussi le fichier robots.txt
+  sitemapSize: 5000, // Limite le nombre d’URLs par fichier sitemap (par défaut 5000)
+};
