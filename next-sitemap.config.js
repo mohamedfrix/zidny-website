9a0e58d881aa5f5c83017f853997e4cd8e63c8f9
Tabti Ali <ma_tabti@esi.dz>
2025-07-19 14:40:21 +0100

    robtot.txt sitemap.xml configuration v4

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-// next-sitemap.config.js
-module.exports = {
-  siteUrl: 'https://www.zidnyagency.com', // Remplace par ton vrai nom de domaine
-  generateRobotsTxt: true, // Génère aussi le fichier robots.txt
-  sitemapSize: 5000, // Limite le nombre d’URLs par fichier sitemap (par défaut 5000)
-};
